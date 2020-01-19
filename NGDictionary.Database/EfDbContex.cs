b97528bf3c8ebcc777a64d629ee347037439cbdc
Serhii Chervonyi <chervoniys@gmail.com>
2020-01-19 22:21:36 +0200

    Implement EFDbContex

--- NGDictionary.Database/EfDbContex.cs
+++ NGDictionary.Database/EfDbContex.cs
@@ -1,8 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using NGDictionary.Dto;
+using System;
 
 namespace NGDictionary.Database
 {
-    public class EFDbContex
+    public class EFDbContex : DbContext
     {
+        public EFDbContex(DbContextOptions<EFDbContex> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Dictionary> Dictionaries { get; set; }
+
+        public DbSet<DictionaryMeta> DictionariesMeta { get; set; }
+
+        public DbSet<Word> Words { get; set; }
     }
 }
