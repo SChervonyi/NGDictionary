b825e5b856e5a704b783451813b626e72dde8830
Serhii Chervonyi <chervoniys@gmail.com>
2020-01-21 09:56:02 +0200

    Add IUnitOfWork interface

---
+++ NGDictionary.Database/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using NGDictionary.Database.Interfaces.Repositories;
+using System;
+
+namespace NGDictionary.Database.Interfaces
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IDictionaryRepository DictionaryRepository { get; }
+
+        IUserRepository UserRepository { get; }
+
+        IWordRepository WordRepository { get; }
+
+        void Save();
+    }
+}
