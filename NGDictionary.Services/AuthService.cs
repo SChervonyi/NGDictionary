c333bd14d04531a067a15f0dd1597a0051fea028
Serhii Chervonyi <chervoniys@gmail.com>
2020-01-22 23:11:32 +0200

    Add NGDictionary.Services project

---
+++ NGDictionary.Services/AuthService.cs
@@ -0,0 +1,11 @@
+﻿using NGDictionary.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NGDictionary.Services
+{
+    public class AuthService: IAuthService
+    {
+    }
+}
