a30435d0b8dc765ae8a5cd64f027532f841130c5
Serhii Chervonyi <chervoniys@gmail.com>
2020-02-01 19:06:21 +0200

    Implement AuthController

--- NGDictionary/Controllers/AuthController.cs
+++ NGDictionary/Controllers/AuthController.cs
@@ -2,14 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NGDictionary.Dto;
+using NGDictionary.Services.Interfaces;
 
 namespace NGDictionary.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    [AllowAnonymous]
+    public class AuthController : Controller
     {
+        private IAuthService authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost]
+        public ActionResult<User> Post(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            authService.AddUser(user);
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            authService.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
