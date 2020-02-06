0865a591c430089e4ea99c9b0a4680133eab60de
Serhii Chervonyi <chervoniys@gmail.com>
2020-02-06 07:31:44 +0200

    Implement submitForm method for user registration

--- NGDictionary/ClientApp/src/app/modules/auth/page/register/register.component.ts
+++ NGDictionary/ClientApp/src/app/modules/auth/page/register/register.component.ts
@@ -1,5 +1,8 @@
+import { AuthService } from '@core/service/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Errors } from '@core/models/errors.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,9 +12,14 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 export class RegisterComponent implements OnInit {
 
   authForm: FormGroup;
+  title: String = '';
+  errors: Errors = {errors: {}};
+  isSubmitting = false;
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
   ) {
         // use FormBuilder to create a form group
         this.authForm = this.fb.group({
@@ -24,4 +32,19 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  submitForm() {
+    this.isSubmitting = true;
+    this.errors = {errors: {}};
+
+    const credentials = this.authForm.value;
+    this.authService
+      .signUp(credentials)
+      .subscribe(
+        data => this.router.navigateByUrl('/'),
+        err => {
+          this.errors = err;
+          this.isSubmitting = false;
+        }
+    );
+  }
 }
