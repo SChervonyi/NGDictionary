eb99ecc15c4d270df5c578fa0ccb9838db18accc
Serhii Chervonyi <chervoniys@gmail.com>
2020-02-27 09:49:27 +0200

    Add DB Initialization

--- NGDictionary/Program.cs
+++ NGDictionary/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using NGDictionary.Database;
+using NGDictionary.Helpers;
 
 namespace NGDictionary
 {
@@ -13,7 +16,28 @@ namespace NGDictionary
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            //Seed database
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var databaseInitializer = services.GetRequiredService<IDatabaseInitializer>();
+                    databaseInitializer.SeedAsync().Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogCritical(LoggingEvents.INIT_DATABASE, ex, LoggingEvents.INIT_DATABASE.Name);
+
+                    throw new Exception(LoggingEvents.INIT_DATABASE.Name, ex);
+                }
+
+                host.Run();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
